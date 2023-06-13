757dcebb99cc62ff233cb8acd1b68a6d93a564b6
LeviOS31 <58821386+LeviOS31@users.noreply.github.com>
2023-06-13 15:25:30 +0200

    GGGGGG

--- village/script/Leave.gd
+++ village/script/Leave.gd
@@ -1,19 +1,17 @@
 extends Area2D
 
-
+signal tran
 
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _physics_process(delta):
+	if $"../../transitionscene/Timer".time_left < 0.1 && $"../../transitionscene/Timer".time_left > 0.00001:
+		emit_signal("tran")
 
 func _on_Leave_area_entered(area):
 		$"../../transitionscene".transition_to_black()
 		yield(self, "tran")
 		Global.playerlocation = null
-		Coordinates.VillageCoord = $"../player".position
+		Coordinates.City1Coord = $"../player".position
 		get_tree().change_scene("res://worlds/"+self.name+".tscn")
