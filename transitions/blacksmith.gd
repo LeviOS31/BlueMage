31ab99e62f09fe41cbe25a5a58aacf8da6579558
LeviOS31 <58821386+LeviOS31@users.noreply.github.com>
2023-06-16 15:04:35 +0200

    HHHHHHHHH

--- transitions/blacksmith.gd
+++ transitions/blacksmith.gd
@@ -1,17 +1,27 @@
 extends Area2D
 
 signal entering
+signal tran
 var entered: bool = false
 
 func _ready():
 	pass # Replace with function body.
+	
 
+func _physics_process(delta):
+	if $"../../../transitionscene/Timer".time_left < 0.1 && $"../../../transitionscene/Timer".time_left > 0.00001:
+		emit_signal("tran")
 
 func _input(event):
 	if(event.is_action_pressed("Interact")):
 		if(entered == true):
 			emit_signal("entering");
+			$"../../../transitionscene".transition_to_black()
+			yield(self,"tran")
 			$"../../player".global_position = $exithouse.global_position;
+			$"../../../transitionscene/Timer".start(1)
+			yield($"../../../transitionscene/Timer", "timeout")
+			$"../../../transitionscene".transition_to_normal()
 
 
 
